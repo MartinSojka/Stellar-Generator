e501577a92f648c207da2959c8af6598969b21b5
Martin Sojka <github-official@martin-sojka.de>
2015-11-24 17:59:27 +0100

    Minor constant fixes / expansion

--- src/main/java/de/vernideas/space/data/Constant.java
+++ src/main/java/de/vernideas/space/data/Constant.java
@@ -8,12 +8,22 @@ public final class Constant {
 	public static final double TIME_UNIT = 1.0;
 	/** Planetary mass unit: 1 kg */
 	public static final double MASS_UNIT = 1.0;
-	/** Interplanetary speed unit; mAU/minute. This is a very large speed, about 125 units = light speed */
+	/** In m/s */
 	public static final double SPEED_UNIT = DISTANCE_UNIT / TIME_UNIT;
-	/** Interplanetary acceleration unit: mAU/minute². Very large unit and not usable for display */
+	/** In m/s^2 */
 	public static final double ACCELERATION_UNIT = SPEED_UNIT / TIME_UNIT;
-	/** Minutes per mAU; usable for display of interplanetary travel information */
+	/** In s/m */
 	public static final double PACE_UNIT = TIME_UNIT / DISTANCE_UNIT;
+	/** In N = kg * m/s^2 */
+	public static final double FORCE_UNIT = ACCELERATION_UNIT * MASS_UNIT;
+	/** In J = N * m = kg * m^2 / s^2 */
+	public static final double ENERGY_UNIT = FORCE_UNIT * DISTANCE_UNIT;
+	/** In W = J / s = kg * m^2 / s^3 */
+	public static final double LUM_UNIT = ENERGY_UNIT / TIME_UNIT;
+	/** in kg/m^3 */
+	public static final double DENSITY_UNIT = MASS_UNIT / DISTANCE_UNIT / DISTANCE_UNIT / DISTANCE_UNIT;
+	/** In Pa = N / m^2 = kg / (m * s^2) */
+	public static final double PRESSURE_UNIT = FORCE_UNIT / DISTANCE_UNIT / DISTANCE_UNIT;
 	
 	/**
 	 * Gravitational constant (in m^3 kg^-1 s^-2)
@@ -41,7 +51,7 @@ public final class Constant {
 	/** Solar radius (in distance units) */
 	public static final double SOLAR_DIAMETER = 1.391e9 / DISTANCE_UNIT;
 	/** Solar luminosity (in W) */
-	public static final double SOLAR_LUM = 3.846e26;
+	public static final double SOLAR_LUM = 3.846e26 / LUM_UNIT;
 	/** Solar mass (in mass units) */
 	public static final double SOLAR_MASS = 1.98855e30 / MASS_UNIT;
 	public static final double SOLAR_TEMPERATURE = 5778.0;
@@ -59,37 +69,40 @@ public final class Constant {
 	
 	/** Average temperature of the universe, in Kelvin */
 	public static final double UNIVERSE_TEMPERATURE = 2.73;
-	public static final double UNIVERSE_AGE = 1.3799e9 * 31557600.0; // Julian years for astronomy values
+	public static final double UNIVERSE_AGE = 1.3799e9 * 31557600.0 / TIME_UNIT; // Julian years for astronomy values
 	
-	/** Earth surface gravity in m/s^2 */
-	public static final double EARTH_SURFACE_GRAVITY = 9.807;
-	public static final double EARTH_DIAMETER = 6375098 * 2; // Calculated from gravity (9.807 m/s^2) and mass
+	/** Earth surface gravity (acceleration) in m/s^2 */
+	public static final double EARTH_SURFACE_GRAVITY = 9.807 / ACCELERATION_UNIT;
+	/** Earth diameter, calculated from gravity (9.807 m/s^2) and mass */
+	public static final double EARTH_DIAMETER = 6375098 * 2 / DISTANCE_UNIT; 
+	/** Earth's standard atmospheric pressure */
+	public static final double EARTH_ATM_PRESSURE = 101325.0 / PRESSURE_UNIT;
 	
 	// Tidal locking constants: 6x10^10 years times 3x10^10 kg m^-1 s^-2 for rocky,
 	// times 4x10^9 N m^2 for icy worlds, and result in galactic years.
 	public static final double TIDAL_LOCKING_ROCKY = 6e10 * 3e10 / GALACTIC_YEAR;
 	public static final double TIDAL_LOCKING_ICY = 6e10 * 4e9 / GALACTIC_YEAR;
 	
-	/** 8.3144621 m^2 kg s^-2 K^-1 mol^-1 with the base time unit */
-	public static final double MOLAR_GAS = 8.314459848 * TIME_UNIT * TIME_UNIT;
+	/** 8.3144621 m^2 kg s^-2 K^-1 mol^-1  */
+	public static final double MOLAR_GAS = 8.314459848 / ENERGY_UNIT;
 	
 	/** Rough upper limit of terrestrial planets, in Yg */
-	public static final double MAX_TERRESTRIAL_MASS = 5e25;
-	public static final double MIN_TERRESTRIAL_MASS = 1e23;
-	public static final double MIN_MOON_MASS = 1e19;
-	public static final double EARTH_MASS = 5.97219e24;
+	public static final double MAX_TERRESTRIAL_MASS = 5e25 / MASS_UNIT;
+	public static final double MIN_TERRESTRIAL_MASS = 1e23 / MASS_UNIT;
+	public static final double MIN_MOON_MASS = 1e19 / MASS_UNIT;
+	public static final double EARTH_MASS = 5.97219e24 / MASS_UNIT;
 	
 	public static final double YOTTAGRAM = 1e21 / MASS_UNIT;
 	
 	// Earth - estimated density limits for core, mantle and crust.
 	// Used with linear interpolation
-	public static final double EARTH_CORE_MAX_DENSITY = 13300.0;
-	public static final double EARTH_CORE_MIN_DENSITY = 10000.0;
-	public static final double EARTH_MANTLE_MAX_DENSITY = 5226.0;
-	public static final double EARTH_MANTLE_MIN_DENSITY = 4234.0;
-	public static final double EARTH_CRUST_MAX_DENSITY = 3538.0;
-	public static final double EARTH_CRUST_MIN_DENSITY = 2300.0;
+	public static final double EARTH_CORE_MAX_DENSITY = 13300.0 / DENSITY_UNIT;
+	public static final double EARTH_CORE_MIN_DENSITY = 10000.0 / DENSITY_UNIT;
+	public static final double EARTH_MANTLE_MAX_DENSITY = 5226.0 / DENSITY_UNIT;
+	public static final double EARTH_MANTLE_MIN_DENSITY = 4234.0 / DENSITY_UNIT;
+	public static final double EARTH_CRUST_MAX_DENSITY = 3538.0 / DENSITY_UNIT;
+	public static final double EARTH_CRUST_MIN_DENSITY = 2300.0 / DENSITY_UNIT;
 	// Earth - estimated radius of core and mantle (crust = planet radius) in m
-	public static final double EARTH_CORE_RADIUS = 3400000.0;
-	public static final double EARTH_MANTLE_RADIUS = 6335000.0;
+	public static final double EARTH_CORE_RADIUS = 3400000.0 / DISTANCE_UNIT;
+	public static final double EARTH_MANTLE_RADIUS = 6335000.0 / DISTANCE_UNIT;
 }
